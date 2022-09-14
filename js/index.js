4cf310aa14c6e464828d8d4ed2a38478f00b7944
nazcor <nascoron@gmail.com>
2022-09-13 20:18:07 -0700

    Working functions for number buttons, decimal button, and clear button

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+function insertZero() {
+    document.querySelector('#output').value += '0';
+};
+
+function insertOne() {
+    document.querySelector('#output').value += '1';
+};
+
+function insertTwo() {
+    document.querySelector('#output').value += '2';
+};
+
+function insertThree() {
+    document.querySelector('#output').value += '3';
+};
+
+function insertFour() {
+    document.querySelector('#output').value += '4';
+};
+
+function insertFive() {
+    document.querySelector('#output').value += '5';
+};
+
+function insertSix() {
+    document.querySelector('#output').value += '6';
+};
+
+function insertSeven() {
+    document.querySelector('#output').value += '7';
+};
+
+function insertEight() {
+    document.querySelector('#output').value += '8';
+};
+
+function insertNine() {
+    document.querySelector('#output').value += '9';
+};
+
+function insertDecimal() {
+    document.querySelector('#output').value += '.';
+};
+
+function clearOutput() {
+    document.querySelector('#output').value = '';
+};
+
+function deleteOutput() {
+    
+};
